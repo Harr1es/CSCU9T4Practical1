d39a73bfbc51ba71b8325dd86ef48fc2b0bc4de1
Harries Gora <123955390+Harr1es@users.noreply.github.com>
2023-04-06 23:49:51 +0100

    Add files via upload

--- src/main/java/com/stir/cscu9t4practical1/TrainingRecord.java
+++ src/main/java/com/stir/cscu9t4practical1/TrainingRecord.java
@@ -30,6 +30,41 @@ public class TrainingRecord {
             }
        return result;
    } // lookupEntry
+// This method takes in a day, month, and year as input and returns a string 
+// containing all the entries in a list that have the same date as the input.
+
+public String FindAll_by_date(int d, int m, int y) {  // Question 2// 
+    // Create a new list iterator for the "tr" list
+    ListIterator<Entry> iter = tr.listIterator();
+    
+    // Initialize an empty string to hold the result
+    String result = "";
+    
+    // Loop through each entry in the list
+    while (iter.hasNext()) {
+        // Get the current entry from the iterator
+        Entry current = iter.next();
+        
+        // Check if the current entry's day, month, and year match the input
+        if (current.getDay() == d && current.getMonth() == m && current.getYear() == y) {
+            // If the entry matches, add its text to the result string
+            result += current.getEntry() + ' ';
+        }
+    }
+    
+    // Return the result string
+    return result;
+}
+
+public void removeEntry(String n, int d, int m, int y) {
+	   ListIterator<Entry> iter = tr.listIterator();
+    while (iter.hasNext()) {
+       Entry current = iter.next();
+       if ((current.getName()).compareToIgnoreCase(n) == 0 && current.getDay()==d && current.getMonth()==m && current.getYear()==y) 
+     	  iter.remove();
+    }
+}
+
    
    // Count the number of entries
    public int getNumberOfEntries(){
--- src/main/java/com/stir/cscu9t4practical1/TrainingRecordGUI.java
+++ src/main/java/com/stir/cscu9t4practical1/TrainingRecordGUI.java
@@ -27,6 +27,21 @@ public class TrainingRecordGUI extends JFrame implements ActionListener {
     private JLabel labdist = new JLabel(" Distance (km):");
     private JButton addR = new JButton("Add");
     private JButton lookUpByDate = new JButton("Look Up");
+    private JButton FindAllByDate = new JButton("Find All By Date"); // addition of the new button//
+    private JButton remove = new JButton("Remove");
+    private  JLabel recovery = new JLabel ("Recovery");
+    private JLabel repetitions = new JLabel ("Repetitions");
+    private JLabel terrain = new JLabel ("Terrain");
+    private JLabel tempo = new JLabel ("Tempo");
+    private JLabel where = new JLabel ("Where");
+    private JTextField recoveryText = new JTextField(3);
+    private JTextField repetitionsText = new JTextField(3);
+    private JTextField terrainText = new JTextField(3);
+    private JTextField tempoText = new JTextField(3);
+    private JTextField whereText = new JTextField(10);
+    
+    String [] Sports = {"Choose a sport", "SprintEntry", "CycleEntry", "SwimEntry"}; 
+    JComboBox comboBox = new JComboBox<String>(Sports);
 
     private TrainingRecord myAthletes = new TrainingRecord();
 
@@ -65,7 +80,11 @@ public class TrainingRecordGUI extends JFrame implements ActionListener {
         add(dist);
         dist.setEditable(true);
         add(addR);
+        add(FindAllByDate);
+        FindAllByDate.addActionListener(this); // neeeded for button to function//
         addR.addActionListener(this);
+        add(remove);
+        remove.addActionListener(this);
         add(lookUpByDate);
         lookUpByDate.addActionListener(this);
         add(outputArea);
@@ -73,6 +92,10 @@ public class TrainingRecordGUI extends JFrame implements ActionListener {
         setSize(720, 200);
         setVisible(true);
         blankDisplay();
+        add (comboBox);
+        
+        
+        
 
         // To save typing in new entries while testing, uncomment
         // the following lines (or add your own test cases)
@@ -81,18 +104,50 @@ public class TrainingRecordGUI extends JFrame implements ActionListener {
 
     // listen for and respond to GUI events 
     public void actionPerformed(ActionEvent event) {
+    	if (event.getSource()== comboBox) {
+    		System.out.println(comboBox.getSelectedItem());
+    	}
         String message = "";
         if (event.getSource() == addR) {
             message = addEntry("generic");
         }
         if (event.getSource() == lookUpByDate) {
             message = lookupEntry();
+            
         }
+       if (event.getSource() == FindAllByDate) { //*//
+      	message = FindAll_by_date();
+      	
+       }
+       if (event.getSource() == remove) {
+       	message = remove();}
+       
         outputArea.setText(message);
         blankDisplay();
     } // actionPerformed
 
-    public String addEntry(String what) {
+    public String FindAll_by_date() { /* additton of statements for the action performed */ //Question 3//
+    	int m = Integer.parseInt(month.getText());
+    	int d = Integer.parseInt(day.getText());
+    	int y = Integer.parseInt(year.getText());
+    	outputArea.setText( "loading.... ");
+    	String message = myAthletes.FindAll_by_date (d,m,y);
+    	
+		// TODO Auto-generated method stub
+		return message;
+	}
+
+
+public String remove() {
+	String message = "Entry removed"; 
+	String n = name.getText();
+    int m = Integer.parseInt(month.getText());
+    int d = Integer.parseInt(day.getText());
+    int y = Integer.parseInt(year.getText());
+    myAthletes.removeEntry(n,d,m,y);
+    return message;} 
+
+	public String addEntry(String what) {
         String message = "Record added\n";
         System.out.println("Adding "+what+" entry to the records");
         String n = name.getText();
